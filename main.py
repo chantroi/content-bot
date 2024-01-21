7a781efcf48e4852c3ed554cd2849797c001ed17
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 20:06:45 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import discord, os
-from discord.ext import commands
+import discord
 from init import bot_token
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = discord.Bot()
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+@bot.user_command(name="Say Hello")
+async def hi(ctx, user):
+    await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
 
 bot.run(bot_token)
