d31b0230577587b2088bac9b38c14935e2e942b4
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 17:32:10 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord, os
 from discord.ext import commands
+from init import bot_token
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
-bot.run(os.getenv("TOKEN"))
+bot.run(bot_token)
