4199bdda28122a106993c59367257011e3db1757
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 20:13:36 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import pycord
+from discord.ext import commands
 from init import bot_token
+import discord
 
-bot = pycord.Bot()
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+    description="Content Download",
+    intents=intents,)
 
 @bot.slash_command()
 async def hello(ctx, name: str = None):
