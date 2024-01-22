ae6734b77aa3fd47962fd9e3a979a6ac5b4ff3e5
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 10:53:02 +0700

    update commands/tikdou.py

--- commands/tikdou.py
+++ commands/tikdou.py
@@ -2,24 +2,26 @@ from discord.ext import commands
 import discord
 import requests
 import os
+import re
 
 dapi = os.getenv("DAPI")
 
 class TTDY(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command
-    async def on_message(self, ctx):
-        await ctx.typing()
-        url = re.search(r"(?P<url>https?://[^\s]+)", ctx.message).group("url")
-        r = requests.get(dapi = "/tikdou", params={"url": url}).json()
+    
+    @commands.command()
+    async def ttdy(self, ctx):
+        await ctx.trigger_typing()
+        url = re.search(r"(?P<url>https?://[^\s]+)", ctx.message.content).group("url")
+        r = requests.get(dapi + "/tikdou", params={"url": url}).json()
         dl_link = r.get("url")
         if isinstance(dl_link, list):
             for link in dl_link:
-                await ctx.respond(file=discord.File(link))
+                await ctx.send(file=discord.File(link))
         else:
-            await ctx.respond(file=discord.File(dl_link))
-        await ctx.delete()
-    
+            await ctx.send(file=discord.File(dl_link))
+        await ctx.message.delete()
+
 def setup(bot):
     bot.add_cog(TTDY(bot))
