a8304f247ad108d41a9d106b17cbd691eabed952
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 09:51:50 +0700

    update commands/basic.py

--- commands/basic.py
+++ commands/basic.py
@@ -4,6 +4,7 @@ basic = discord.SlashCommandGroup("basic")
 
 @basic.command()
 async def content(ctx):
+    await ctx.typing()
     await ctx.respond("Đây là Content Download phiên bản Discord")
     
 def setup(bot):
