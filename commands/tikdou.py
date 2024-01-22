f1c11e25b5d31686582319b2b76bc28e0f4e4e31
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 09:31:53 +0700

    update commands/tikdou.py

---
+++ commands/tikdou.py
@@ -0,0 +1,16 @@
+import discord
+import requests
+import os
+
+dapi = os.getenv("DAPI")
+
+async def tikdou(ctx):
+    url = re.search(r"(?P<url>https?://[^\s]+)", ctx.content).group("url")
+    r = requests.get(dapi = "/tikdou", params={"url": url}).json()
+    dl_link = r.get("url")
+    if isinstance(dl_link, list):
+        for link in dl_link:
+            await ctx.respond(file=discord.File(link))
+    else:
+        await ctx.respond(file=discord.File(dl_link))
+    await ctx.delete()
