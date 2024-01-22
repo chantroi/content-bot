4d82f33b56290e62e97562ff3be04531c4aaeab2
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 10:23:26 +0700

    update commands/tikdou.py

--- commands/tikdou.py
+++ commands/tikdou.py
@@ -4,17 +4,21 @@ import os
 
 dapi = os.getenv("DAPI")
 
-async def tikdou(ctx):
-    await ctx.typing()
-    url = re.search(r"(?P<url>https?://[^\s]+)", ctx.content).group("url")
-    r = requests.get(dapi = "/tikdou", params={"url": url}).json()
-    dl_link = r.get("url")
-    if isinstance(dl_link, list):
-        for link in dl_link:
-            await ctx.respond(file=discord.File(link))
-    else:
-        await ctx.respond(file=discord.File(dl_link))
-    await ctx.delete()
+class TTDY:
+    def __init__(self, bot):
+        self.bot = bot
+    @bot.listen()
+    async def tikdou(ctx):
+        await ctx.typing()
+        url = re.search(r"(?P<url>https?://[^\s]+)", ctx.content).group("url")
+        r = requests.get(dapi = "/tikdou", params={"url": url}).json()
+        dl_link = r.get("url")
+        if isinstance(dl_link, list):
+            for link in dl_link:
+                await ctx.respond(file=discord.File(link))
+        else:
+            await ctx.respond(file=discord.File(dl_link))
+        await ctx.delete()
     
 def setup(bot):
-    bot.add_listener(tikdou)
+    bot.add_cogs(TTDY(bot))
