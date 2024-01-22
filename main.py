9e5f7057944b2b9a5a6dce65723f237dc11c6f87
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 10:29:36 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from init import bot_token
+from discord.ext import commands
 import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Bot(
+bot = commands.Bot(
     description="Content Download",
     intents=intents,)
 
