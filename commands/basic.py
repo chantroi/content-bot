208b9d8802f2c6c9aedec060c64f590b4f7bb32d
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-22 10:40:31 +0700

    update commands/basic.py

--- commands/basic.py
+++ commands/basic.py
@@ -1,7 +1,8 @@
-import discord
 from discord.ext import commands
 
-class Basic:
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
     @commands.slash_command()
     async def content(ctx):
         await ctx.respond("Đây là Content Download phiên bản Discord")
