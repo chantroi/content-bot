bbac4c84e64eb54fec6a58af178ffe1e69bfea1e
chantroi <tonglanhchua@gmail.com>
2024-02-02 15:49:41 +0700

    update environ.py

---
+++ environ.py
@@ -0,0 +1,11 @@
+from types import SimpleNamespace
+from deta import Deta
+import os, requests, json
+
+def res():
+    secret = requests.get(os.getenv("SECRET")).text
+    return json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
+res = res()
+deta = Deta(res.key.web_collection)
+bot_token = res.bot.discord
+tg_token = res.bot.td_tg
