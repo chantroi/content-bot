10ab659e578acc643a2640ba6d2dc7f92e16ebed
chantroi <tonglanhchua@gmail.com>
2024-02-02 15:49:48 +0700

    update environ.py

--- environ.py
+++ environ.py
@@ -5,6 +5,7 @@ import os, requests, json
 def res():
     secret = requests.get(os.getenv("SECRET")).text
     return json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
+    
 res = res()
 deta = Deta(res.key.web_collection)
 bot_token = res.bot.discord
