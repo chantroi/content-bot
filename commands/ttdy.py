81bed6ae8c5bced588b62d969ad59285ca999a7d
chantroi <tonglanhchua@gmail.com>
2024-03-06 22:16:44 +0700

    update commands/ttdy.py

--- commands/ttdy.py
+++ commands/ttdy.py
@@ -2,18 +2,6 @@ from discord.ext import commands
 from environ import dapi, tg_token
 import requests, re, io, discord
 
-def tg_send(original, link):
-    BUTTONS = [
-    {"text": "Original", "url": original},
-    {"text": "Youtube", "url": "https://youtube.com/@tiktokdouyin-share"},
-    {"text": "Channel", "url": "https://t.me/contentdownload"},
-    ]
-    KEYBOARD = {"inline_keyboard": [BUTTONS]}
-    VIDEO = {"chat_id": -1001559828576, "video": link, "reply_markup": KEYBOARD, "caption": "_*Discord Cross-Platform*_", "parse_mode": "Markdown"}
-    URL = f"https://api.telegram.org/bot{tg_token}/sendVideo?chat_id=-1001559828576"
-    response = requests.post(URL, json=VIDEO)
-    print(response.json())
-
 class TTDy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -37,9 +25,8 @@ class TTDy(commands.Cog):
                 file.name = "video.mp4"
                 await message.channel.send(file=discord.File(file))
                 if message.channel == self.bot.get_channel(1095488012638507028):
-                    #req = requests.post(f"https://api.telegram.org/bot{tg_token}/sendMessage", params={"chat_id": -1001559828576, "text": url})
-                    #print(req.text)
-                    tg_send(url, dl_link)
+                    req = requests.post(f"https://api.telegram.org/bot{tg_token}/sendMessage", params={"chat_id": -1001559828576, "text": url})
+                    print(req.text)
                     await message.delete()
             
 def setup(bot):
