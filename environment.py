481c8ce133e5de0f7b39d7d3f150eb7aa8a63409
chantroi <tonglanhchua@gmail.com>
2024-06-06 06:28:22 +0700

    Update environment.py

--- environment.py
+++ environment.py
@@ -6,5 +6,5 @@ from types import SimpleNamespace
 secret = requests.get(os.getenv("SECRET"), timeout=99).text
 res = json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
 dapi = res.api.dapi
-bot_token = res.bot.discord
+bot_token = res.bot.contentdiscord
 tg_token = res.bot.tiktokdouyin
