c5e29d4aa7cdb70385acb8ea323bc9d109bfbdc0
AzureVM <tonglanhchua@gmail.com>
2024-07-20 14:05:20 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- src/environment.py
+++ src/environment.py
@@ -4,7 +4,7 @@ import requests
 
 secret = requests.get(os.getenv("SECRET"), timeout=99)
 res = secret.json()
-dapi = res['api']['dapi']['td']
-bot_token = res['bot']['contentdiscord']
-tg_token = res['bot']['tiktokdouyin']
-google_api = res['key']['google_ai']
+dapi = res["api"]["dapi"]["td"]
+bot_token = res["bot"]["contentdiscord"]
+tg_token = res["bot"]["tiktokdouyin"]
+google_api = res["key"]["google_ai"]
