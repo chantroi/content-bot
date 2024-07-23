ccbed15b65b9166e6c54afb6a83d22ec20fe789a
AzureVM <tonglanhchua@gmail.com>
2024-07-23 15:07:43 +0700

    update environment.py

--- src/environment.py
+++ src/environment.py
@@ -5,6 +5,6 @@ import requests
 secret = requests.get(os.getenv("SECRET"), timeout=99)
 res = secret.json()
 dapi = res["api"]["dapi"]["td"]
-bot_token = res["bot"]["contentdiscord"]
-tg_token = res["bot"]["tiktokdouyin"]
-google_api = res["key"]["google_ai"]
+bot_token = res["access"]["discord"]
+tg_token = res["access"]["telegram"]["td"]
+google_api = res["access"]["google_ai"]
