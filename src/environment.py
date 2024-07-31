0cd491f870f27465868633ab90bae1019aafc127
New Horizon <tonglanhchua@gmail.com>
2024-07-31 13:47:19 +0000

    update environment.py

--- src/environment.py
+++ src/environment.py
@@ -5,6 +5,6 @@ import requests
 secret = requests.get(os.getenv("SECRET"), timeout=99)
 res = secret.json()
 dapi = res["api"]["dapi"]["td"]
-bot_token = res["access"]["discord"]
+bot_token = res["access"]["discord"][0]
 tg_token = res["access"]["telegram"]["td"]
 google_api = res["access"]["google_ai"]
